2e828bc88cfefca8d76e74466687a5ea3c828588
Dominic Hawton <dominic.hawton@mq.edu.au>
2020-11-13 09:54:34 +1100

    feat: init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider, theme, extendTheme, Box } from '@chakra-ui/core';
+
+import { colors, radii, fonts } from '../theme';
+
+const customTheme = extendTheme({ colors, radii, fonts });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={customTheme}>
+			<Box backgroundColor="#f9f9f9">
+				<Component {...pageProps} />
+			</Box>
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
